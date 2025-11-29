0f67bd610e0e2476b2fa8b1fc64a45449dae68be
antoine-lepage <antoine.lepage98@gmail.com>
2025-11-29 15:18:20 +0100

    Correct variable usage and output in Caracteres.java
    Fixed variable declaration and corrected string output.

--- src/fr/extia/mentorat/exercice/Caracteres.java
+++ src/fr/extia/mentorat/exercice/Caracteres.java
@@ -8,20 +8,24 @@ package fr.extia.mentorat.exercice;
 
 public class Caracteres {
     public static void main(String[] args) {
+        // Afficher la concaténation des variables a et b (sortie attendue : aabc)
         char a = 'a';
         String b = "abc";
 
         System.out.println(a + b);
 
         //---------------------------------
+        // Afficher le caractère en correspondant au code 97
         char b = 97;
         System.out.println(b);
 
         //---------------------------------
+        // Afficher le caractère c 
         char c = "c";
         System.out.println(c);
+        
         //---------------------------------
-
+        // Afficher le texte suivant dans la console
         char d = """
                 Ceci est mon texte
                 Merci d'en respcter la nature
@@ -33,9 +37,10 @@ public class Caracteres {
         System.out.println(d);
 
         //---------------------------------
+        // Afficher le plus beau prénom du monde
         {
             String prenom = 'Julie';
-            System.out.println(prenom);
         }
+        System.out.println(prenom);
     }
 }
